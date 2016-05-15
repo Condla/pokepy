de3b2372695388431cbad0cf85888fdabef159f0
Stefan K. Dunkler <stefan.dun@gmail.com>
2016-05-15 20:59:06 +0200

    fix json parse bug

--- pokepy/__init__.py
+++ pokepy/__init__.py
@@ -16,28 +16,43 @@ POKEAPI = 'https://pokeapi.co/api/v2/{path}/{id}'
 
 def get_api_path(arguments):
     '''
-Get pokemon or type or ability command
+    Get pokemon or type or ability command from command line
+    arguments.
     '''
     paths = ['pokemon', 'type', 'ability']
     for path in paths:
-        path = arguments[path]
-        if path:
+        if arguments[path]:
             break
     return path
 
 
 def get_id(arguments):
+    '''
+    Get id from command line arguments.
+    '''
     return arguments['--id']
 
 
 def call_pokeapi(path, id_number, key='name'):
+    '''
+    Call the RESTful PokeAPI and parse the response. If pokemon, ability or
+    type ids are not found than the error message detail is returned.
+    '''
     url = POKEAPI.format(path=path, id=id_number)
     response = requests.get(url)
-    res = response.json()[key]
+    response_json = response.json()
+    try:
+        res = response_json[key]
+    except:
+        res = response_json['detail']
+
     return res
 
 
 def __main__():
+    '''
+    Entrypoint of command line interface.
+    '''
     from docopt import docopt
     arguments = docopt(__doc__, version='0.1.0')
     path = get_api_path(arguments)
